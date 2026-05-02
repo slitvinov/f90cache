3edbf6c53b68fbd755c26c385523e1da504391c6
Sergey Litvinov <slitvinov@g.harvard.edu>
2026-05-01 23:14:14 -0400

    Drop autotools, build with a static Makefile
    Modern macOS/Linux toolchains have all the libc/POSIX features the
    configure script probes for, so the autoconf dance only added moving
    parts. Replaced configure / Makefile.in / config.h.in with a
    hand-written Makefile and a static config.h that asserts the
    HAVE_* set we actually need. install-sh removed.
    
    Build:
      make
      make install PREFIX=$HOME/.local

---
+++ Makefile
@@ -0,0 +1,28 @@
+CC      ?= cc
+CFLAGS  ?= -O2 -Wall
+PREFIX  ?= /usr/local
+
+bindir = $(PREFIX)/bin
+mandir = $(PREFIX)/share/man
+
+OBJS = f90cache.o mdfour.o hash.o execute.o util.o args.o stats.o cleanup.o snprintf.o
+HEADERS = f90cache.h mdfour.h config.h
+
+all: f90cache
+
+f90cache: $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS)
+
+%.o: %.c $(HEADERS)
+	$(CC) -I. $(CFLAGS) -c -o $@ $<
+
+install: f90cache
+	install -d $(DESTDIR)$(bindir)
+	install -m 755 f90cache $(DESTDIR)$(bindir)/
+	install -d $(DESTDIR)$(mandir)/man1
+	install -m 644 f90cache.1 $(DESTDIR)$(mandir)/man1/
+
+clean:
+	rm -f $(OBJS) f90cache
+
+.PHONY: all install clean
---
+++ config.h
@@ -0,0 +1,31 @@
+#ifndef F90CACHE_CONFIG_H
+#define F90CACHE_CONFIG_H
+
+#define _GNU_SOURCE 1
+
+#define HAVE_ASPRINTF       1
+#define HAVE_VASPRINTF      1
+#define HAVE_SNPRINTF       1
+#define HAVE_VSNPRINTF      1
+#define HAVE_C99_VSNPRINTF  1
+#define HAVE_REALPATH       1
+#define HAVE_MKSTEMP        1
+#define HAVE_GETHOSTNAME    1
+#define HAVE_GETPWUID       1
+
+#define HAVE_CTYPE_H        1
+#define HAVE_DIRENT_H       1
+#define HAVE_INTTYPES_H     1
+#define HAVE_PWD_H          1
+#define HAVE_STDINT_H       1
+#define HAVE_STDLIB_H       1
+#define HAVE_STRING_H       1
+#define HAVE_STRINGS_H      1
+#define HAVE_SYS_STAT_H     1
+#define HAVE_SYS_TYPES_H    1
+#define HAVE_SYS_WAIT_H     1
+#define HAVE_UNISTD_H       1
+
+#define STDC_HEADERS 1
+
+#endif
